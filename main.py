b21ea060fdc22de04c78ce996f58badc1377a31e
Connor12858 <connor.godden2006@gmail.com>
2021-10-07 16:14:08 -0400

    Updated to just hand tracking

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 import cv2
-import face_recognition
 import mediapipe as mp
 import numpy as np
 import pyautogui as mouse
-import encryptionSystem as Es
-import data_setup
-import face_encodings as fe
-import setup
 
 
-def program(userName):
-    # Should reply with name
-    setup.speak("Hello {}".format(userName))
-
+def program():
     # Declaring the MediaPipe solutions
     drawing_module = mp.solutions.drawing_utils
     hand_module = mp.solutions.hands
@@ -116,119 +108,5 @@ def program(userName):
     cv2.destroyWindow('Hand Tracking')
 
 
-def face_enter():
-    video_capture = cv2.VideoCapture(0)
-
-    known_face_encodings = fe.face_encode()
-    known_face_names = fe.face_name()
-
-    isAdmin = False
-    isUser = False
-    userName = "guest"
-
-    face_locations = []
-    face_encodings = []
-    face_names = []
-    process_this_frame = True
-
-    while True:
-        # Grab a single frame of video
-        ret, frame = video_capture.read()
-
-        # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
-
-        # Only process every other frame of video to save time
-        if process_this_frame:
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-
-            face_names = []
-            for face_encoding in face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                name = "No Match"
-
-                # # If a match was found in known_face_encodings, just use the first one.
-                # if True in matches:
-                #     first_match_index = matches.index(True)
-                #     name = known_face_names[first_match_index]
-
-                # Or instead, use the known face with the smallest distance to the new face
-                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
-                    userName = name
-                    if "_admin" in name:
-                        isAdmin = True
-                    else:
-                        isUser = True
-                    # break
-
-                face_names.append(name)
-
-        process_this_frame = not process_this_frame
-
-        # Display the results
-        for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
-
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-            # Draw a label with a name below the face
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-        # Display the resulting image
-        cv2.imshow('Face Recognition', frame)
-
-        # Hit 'q' on the keyboard to quit!
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Release handle to the webcam
-    video_capture.release()
-    cv2.destroyWindow('Face Recognition')
-    if isAdmin:
-        setup.speak("Say Admin Password")
-        password = setup.voice()
-        if password == setup.adminPassword:
-            program(userName)
-        else:
-            raise SystemExit
-
-    program(userName)
-
-
 if __name__ == "__main__":
-    dataSetup = open("data\\dataSetUp", "r")
-    dataString = dataSetup.read()
-    dataContents = Es.decrypt_message(dataString)
-
-    if dataContents == "False":
-        print("Setting up data...")
-        data_setup.fileCreation()
-    dataSetup.close()
-
-    '''
-    accs = open("data\\accounts", "r")
-    accsContent = Es.decrypt_message(accs.read())
-
-    if accsContent == "":
-        print("Creating Admin account...")
-        accs.close()
-        setup.createAdmin()
-    '''
-
-    face_enter()
+    program()
